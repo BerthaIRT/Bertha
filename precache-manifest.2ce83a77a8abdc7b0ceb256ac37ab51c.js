1e915d9d0ee9d37f343c4d30deb3f19dd21a5858
johnathankoz <jkozlowski@crimson.ua.edu>
2018-11-30 10:52:46 -0600

    Updates

---
+++ precache-manifest.2ce83a77a8abdc7b0ceb256ac37ab51c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "712804f339a2b9fd5e597b099cff7652",
+    "url": "/website/static/media/jackedwallpaper.712804f3.jpg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "d2e352673c66da88c97c",
+    "url": "/website/static/js/main.d2e35267.chunk.js"
+  },
+  {
+    "revision": "8d63cff5a8bb4851023c",
+    "url": "/website/static/js/1.8d63cff5.chunk.js"
+  },
+  {
+    "revision": "d2e352673c66da88c97c",
+    "url": "/website/static/css/main.2b747b87.chunk.css"
+  },
+  {
+    "revision": "8d63cff5a8bb4851023c",
+    "url": "/website/static/css/1.c8cca59e.chunk.css"
+  },
+  {
+    "revision": "9d481840dc23c05b41d06caf112c22c9",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1632b2c72e2944c4c7739de19481324.js"
+  "/website/precache-manifest.2ce83a77a8abdc7b0ceb256ac37ab51c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
